711ffae946241fc451320b12166a33aff7139600
Andrew Andrianov <andrew@ncrmnt.org>
2019-12-16 16:38:12 +0300

    resetSeqPL2303: Implement iverted pl2303 sequence
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- rumboot_xrun/resetSeqPL2303.py
+++ rumboot_xrun/resetSeqPL2303.py
@@ -4,10 +4,13 @@ class resetSeqPL2303:
     name = "pl2303"
     #Physical port to use
     port = -1
+    invert = 0
     # GPIO0: reset
     # GPIO1: power
 
     def gpio(self, gp, v):
+        if self.invert:
+            v = not v
         os.system("pl2303gpio --port=%d --gpio=%d --out=%d" % (self.port, gp, v))
 
     def __init__(self, port):
@@ -29,3 +32,8 @@ class resetSeqPL2303:
         time.sleep(1)
         self.gpio(1, 0)
         self.gpio(0, 0) 
+
+
+class resetSeqPL2303i(resetSeqPL2303):
+    invert = 1
+    name = "pl2303 (inverted)"
