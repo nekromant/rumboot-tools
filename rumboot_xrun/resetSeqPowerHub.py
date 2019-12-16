fec892e6c8cc858f0320e5fa340705ea2bf80ce7
Andrew Andrianov <andrew@ncrmnt.org>
2019-12-16 16:38:50 +0300

    resetSeqPowerHub: Implement powerhub usb port reset sequence
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- rumboot_xrun/resetSeqPowerHub.py
+++ rumboot_xrun/resetSeqPowerHub.py
@@ -17,3 +17,26 @@ class resetSeqPowerHub:
         os.system("powerhubctl outlet_power_ctl 0");
         time.sleep(1)
         os.system("powerhubctl outlet_power_ctl 1");
+
+class resetSeqPowerHubUsb(resetSeqPowerHub):
+    name = "powerhub_usb_0"
+    usb_port = 0
+    def __init__(self, port):
+        self.port = port
+
+    def resetWithCustomFlags(self, flags=[]):
+        print("Please, power-cycle board")
+
+    def resetToHost(self, flags = []):
+        os.system("powerhubctl port_power_ctl %d 0" % self.usb_port);
+        time.sleep(1)
+        os.system("powerhubctl port_power_ctl %d 1" % self.usb_port);
+
+    def resetToNormal(self, flags = []):
+        os.system("powerhubctl port_power_ctl %d 0" % self.usb_port);
+        time.sleep(1)
+        os.system("powerhubctl port_power_ctl %d 1" % self.usb_port);
+
+class resetSeqPowerHubUsb1(resetSeqPowerHubUsb):
+    name = "powerhub_usb_1"
+    usb_port = 1
