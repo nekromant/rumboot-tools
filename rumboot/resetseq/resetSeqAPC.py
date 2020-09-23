31515ddb9fb0e00f2442d80bd9303758841ecd67
Andrew Andrianov <andrew@ncrmnt.org>
2020-09-23 15:00:12 +0300

    resetseq: Fix rare bugs with APC
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- rumboot/resetseq/resetSeqAPC.py
+++ rumboot/resetseq/resetSeqAPC.py
@@ -19,36 +19,50 @@ class apc(base):
 
 
     def expect(self, f, inw, outw):
+        failures = 0
         while True:
+            if failures > 30:
+                raise Exception("Problem with APC")
             tmp = f.readline()
             if len(tmp) == 0:
-                sleep(0.2)
+                sleep(0.1)
+                failures += 1
                 pass
             if inw in tmp:
                 f.write(outw + "\r")
                 f.flush()
                 return
 
+    def _power(self, on):
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                 s.connect((self.host, self.port))
+                 s.setblocking(0)
+                 if on:
+                     on = 1
+                 else:
+                     on = 2
+                 f = s.makefile('rw', encoding="ascii", errors="replace", newline='\r')
+                 self.expect(f, "User Name :", self.user)
+                 self.expect(f, "Password  :", self.passwd)
+                 self.expect(f, ">", "1")
+                 self.expect(f, ">", "2")
+                 self.expect(f, ">", "1")
+                 self.expect(f, ">", str(self.outlet))
+                 self.expect(f, ">", str(on))
+                 self.expect(f, "Enter 'YES' to continue or <ENTER> to cancel", "YES")
+                 self.expect(f, "Press <ENTER> to continue...", "")
+                 s.close()
+                 sleep(3) #Power bricks have huge caps. Let power stabilize
+
     def power(self, on):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-             s.connect((self.host, self.port))
-             s.setblocking(0)
-             if on:
-                 on = 1
-             else:
-                 on = 2
-             f = s.makefile('rw', encoding="ascii", errors="replace", newline='\r')
-             self.expect(f, "User Name :", self.user)
-             self.expect(f, "Password  :", self.passwd)
-             self.expect(f, ">", "1")
-             self.expect(f, ">", "2")
-             self.expect(f, ">", "1")
-             self.expect(f, ">", str(self.outlet))
-             self.expect(f, ">", str(on))
-             self.expect(f, "Enter 'YES' to continue or <ENTER> to cancel", "YES")
-             self.expect(f, "Press <ENTER> to continue...", "")
-             s.close()
-             sleep(2) #Power bricks have huge caps. Let power stabilize
+        #Sometimes we can connect, but get back nothing
+        retries = 4
+        while True:
+            try:
+                self._power(on)
+                break
+            except:
+                continue
 
     def resetWithCustomFlags(self, flags=[]):
         return self.resetToHost(flags)
