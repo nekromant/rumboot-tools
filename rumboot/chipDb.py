3be0c168e12c9e5bb7cbe59f4a2b0da9f6af7e1b
user <user@stend040321-pc.module.ru>
2021-05-24 17:12:32 +0300

    chipDb class was refactored to read YAML configs

--- rumboot/chipDb.py
+++ rumboot/chipDb.py
@@ -1,16 +1,59 @@
-from rumboot.classLoader import classLoader
+import yaml
+from os import listdir
+from os.path import join, isfile, dirname
 
-class ChipDb(classLoader):
+
+class ChipDb:
+
+    chips = {}
+    params_cfg_name = "chips_parameters.yml"
+
+    def __init__(self, path):
+        if len(self.chips) == 0:
+            path = path.split(".")
+            root = dirname(dirname(__file__))
+            path = join(root, *path)
+            with open(join(path, self.params_cfg_name), 'r') as f:
+                params = yaml.safe_load(f)
+
+            yamles = [
+                f for f in listdir(path) if isfile(join(path, f)) and \
+                f.endswith(".yml") and f != self.params_cfg_name
+            ]
+
+            for yml in yamles:
+                with open(join(path, yml), 'r') as f:
+                    chips = yaml.load_all(f, Loader = yaml.SafeLoader)
+                    for chip in chips:
+                        if chip.chip_id in self.chips:
+                            print(
+                                "Warning! Duplicate for chip",
+                                f"with id [{chip.chip_id}] was found in file [{yml}],",
+                                "please check chip config files"
+                            )
+                        else:
+                            invalid_attrs = \
+                                set(chip.__dict__) - set(params['required'] + params['optional'])
+                            if invalid_attrs:
+                                print(
+                                    f"Found invalid parameters for chip [{chip.name}]: {invalid_attrs}"
+                                )
+                                continue
+                            missing_attrs = \
+                                set(params['required']).difference(set(chip.__dict__))
+                            if missing_attrs:
+                                print(
+                                    f"Found missing parameters for chip [{chip.name}]: {missing_attrs}"
+                                )
+                                continue
+                            self.chips[chip.chip_id] = chip
+
     def __getitem__(self, key):
-        ret = super().__getitem__(key)
-        if ret:
-            return ret
-        for name, c in self.classes.items():
-            if c.chip_id == int(key):
-                return c
+        if key in self.chips:
+            return self.chips[key]
         return None
 
-    def load(self, path):
-        super().load(path)
-        for key,value in self.classes.items():
-            value.name = key
+
+class Chip(yaml.YAMLObject):
+    yaml_tag = '!Chip'
+    yaml_loader = yaml.SafeLoader
