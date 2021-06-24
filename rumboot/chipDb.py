5d28ac93b707c08e8fac37e88e6c7590a66db3b1
user <user@stend040321-pc.module.ru>
2021-06-24 18:38:00 +0300

    some chipDb refactor

--- rumboot/chipDb.py
+++ rumboot/chipDb.py
@@ -25,27 +25,28 @@ class ChipDb:
                 with open(join(path, yml), 'r') as f:
                     chips = yaml.load_all(f, Loader = yaml.SafeLoader)
                     for chip in chips:
-                        if chip.chip_id in self.chips:
-                            print(
-                                "Warning! Duplicate for chip",
-                                f"with id [{chip.chip_id}] was found in file [{yml}],",
-                                "please check chip config files"
-                            )
+                        if chip.chip_id in self.chips and \
+                            self.chips[chip.chip_id].chip_rev == chip.chip_rev:
+                            raise MultipleChipConfigError(chip.chip_id, yml)
                         else:
-                            invalid_attrs = \
+                            excess_attrs = \
                                 set(chip.__dict__) - set(params['required'] + params['optional'])
-                            if invalid_attrs:
-                                print(
-                                    f"Found invalid parameters for chip [{chip.name}]: {invalid_attrs}"
-                                )
-                                continue
+                            if excess_attrs:
+                                raise ExcessAttrsError(excess_attrs, chip.chip_id, yml)
+
                             missing_attrs = \
                                 set(params['required']).difference(set(chip.__dict__))
                             if missing_attrs:
-                                print(
-                                    f"Found missing parameters for chip [{chip.name}]: {missing_attrs}"
-                                )
-                                continue
+                                raise MissingAttrsError(missing_attrs, chip.chip_id, yml)
+
+                            excess_hacks = set(chip.hacks) - set(params['hacks'])
+                            if excess_hacks:
+                                raise ExcessHacksError(excess_hacks, chip.chip_id, yml)
+
+                            missing_hacks = set(params['hacks']).difference(set(chip.hacks))
+                            if missing_hacks:
+                                raise MissingHacksError(missing_hacks, chip.chip_id, yml)
+
                             self.chips[chip.chip_id] = chip
 
     def __getitem__(self, key):
@@ -57,3 +58,37 @@ class ChipDb:
 class Chip(yaml.YAMLObject):
     yaml_tag = '!Chip'
     yaml_loader = yaml.SafeLoader
+
+
+class MultipleChipConfigError(Exception):
+    def __init__(self, chip_id, filename):
+        super().__init__(
+            f"Found duplicate config [{filename}] for chip with ID [{chip_id}]"
+        )
+
+
+class InvalidAttrsError(Exception):
+    def __init__(self, chip_id, filename, postfix):
+        super().__init__(
+            f"Chip with ID [{chip_id}] in file [{filename}] {postfix}"
+        )
+
+
+class MissingHacksError(InvalidAttrsError):
+    def __init__(self, hacks, chip_id, filename):
+        super().__init__(chip_id, filename, f"has missing hacks [{hacks}]")
+
+
+class ExcessHacksError(InvalidAttrsError):
+    def __init__(self, hacks, chip_id, filename):
+        super().__init__(chip_id, filename, f"has excess hacks: {hacks}")
+
+
+class MissingAttrsError(InvalidAttrsError):
+    def __init__(self, attrs, chip_id, filename):
+        super().__init__(chip_id, filename, f"has missing attributes: {attrs}")
+
+
+class ExcessAttrsError(InvalidAttrsError):
+    def __init__(self, attrs, chip_id, filename):
+        super().__init__(chip_id, filename, f"has excess attributes: {attrs}")
