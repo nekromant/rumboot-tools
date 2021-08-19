9028d2cdf564d90252d0b856fe46c8ddabc3bdf5
Andrew Andrianov <andrew@ncrmnt.org>
2021-08-19 18:50:07 +0300

    xflash: Implement 'dumb' flasher and add buggy nor options for oi10
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

---
+++ rumboot/flashing/dumb.py
@@ -0,0 +1,34 @@
+from .base import PartitionBase, FlashDeviceBase
+class FlashDeviceDumb(FlashDeviceBase):
+    def __init__(self):
+        self.erase_size = 1
+        self.write_size = 1
+        self.read_size = 1
+        self.size = 128 * 1024 * 1024
+        self.part = "dumb flash device"
+        super().__init__()
+
+    def _read(self, fd, offset, length, cb = None):
+        raise Exception("DUMB SPLs DO NOT SUPPORT READBACK")
+
+    def _write(self, fd, offset, length, cb = None):
+        if offset != 0:
+            raise Exception(f"DUMB SPLs DO NOT SUPPORT OFFSET != 0 (current is {offset}")
+        self.terminal.wait("boot: Press 'X' and send me the image")
+        self.terminal.write("X".encode("ascii"))
+        if self.terminal.xfer.how != "xmodem" and self.terminal.xfer.how != "xmodem1k":
+            raise Exception("Dumb spls only support xmodem")
+        return self.terminal.xfer.from_file(0, fd, cb, offset, length)
+
+    def _erase(self, offset=0, length=-1, cb = None):
+        raise Exception(f"DUMB SPLs DO NOT SUPPORT ERASING")
+
+    def switchbaud(self, newbaud):
+        raise Exception("Not implemented")
+
+class FlasherDumb(PartitionBase, FlashDeviceDumb):
+    device = "dumb"
+    protocol = "dumb"
+    def __init__(self, terminal, device):
+        FlashDeviceDumb.__init__(self)
+        PartitionBase.__init__(self, terminal)
