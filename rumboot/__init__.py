4122008a6706ca3b894de7daaf8d356d4685d3a2
Andrew Andrianov <andrew@ncrmnt.org>
2021-08-23 14:24:03 +0300

    cmdline: Add a scary copyright notice
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- rumboot/__init__.py
+++ rumboot/__init__.py
@@ -1,7 +1,12 @@
 __version__ = "0.9.18"
 __copyright__ = """
-(C) 2018-2021 Andrew Andrianov <andrew@ncrmnt.org>, RC Module
-https://module.ru
-https://github.com/RC-MODULE
+(C) 2018-2021 Andrew Andrianov <andrew@ncrmnt.org>, STC "Module"
+https://module.ru / https://github.com/RC-MODULE
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
 """
 
